ea03c62ded5097aca6cda8b29c37bfca94103202
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-04 11:45:04 +0100

    Initial Commit

---
+++ FotoLoop/FotoLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoLoopApp.swift
+//  FotoLoop
+//
+//  Created by David Lourenço on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FotoLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FotoLoopUITests/FotoLoopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FotoLoopUITestsLaunchTests.swift
+//  FotoLoopUITests
+//
+//  Created by David Lourenço on 04/10/2024.
+//
+
+import XCTest
+
+final class FotoLoopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
