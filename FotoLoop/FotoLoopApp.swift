c826922e1415f034a4df911be59bcdcb51290547
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-04 12:39:14 +0100

    Add Tabs file.

--- FotoLoop/FotoLoopApp.swift
+++ FotoLoop/FotoLoopApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FotoLoopApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Tabs()
         }
     }
 }
---
+++ FotoLoop/Tabs.swift
@@ -0,0 +1,18 @@
+//
+//  Tabs.swift
+//  FotoLoop
+//
+//  Created by David Lourenço on 04/10/2024.
+//
+
+import SwiftUI
+
+struct Tabs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tabs()
+}
