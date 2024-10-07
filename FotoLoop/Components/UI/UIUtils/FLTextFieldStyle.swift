5c8b89d5b39362704f2db6bcb1105dc67d63e056
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-07 16:54:07 +0100

    Change TextField styling.

--- FotoLoop/Components/UI/UIUtils/FLTextFieldStyle.swift
+++ FotoLoop/Components/UI/UIUtils/FLTextFieldStyle.swift
@@ -11,10 +11,10 @@ import SwiftUI
 struct FLTextFieldStyle: TextFieldStyle {
     func _body(configuration: TextField<Self._Label>) -> some View {
         configuration
-            .padding(12)
+            .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 12))
             .background(Colors.white)
             .cornerRadius(20)
-            .shadow(radius: 3)
+            .shadow(color: Colors.black.opacity(0.2), radius: 8, x: 0, y: 5) 
             .foregroundStyle(Colors.spaceGray500)
     }
 }
