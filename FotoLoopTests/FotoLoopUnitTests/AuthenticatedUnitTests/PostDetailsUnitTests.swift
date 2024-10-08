17ab170f028dff052fab2d9360d1f6bc4d7b326c
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-08 12:53:41 +0100

    Add Unit Test files.

---
+++ FotoLoopTests/FotoLoopUnitTests/AuthenticatedUnitTests/PostDetailsUnitTests.swift
@@ -0,0 +1,27 @@
+//
+//  PostDetailsUnitTests.swift
+//  FotoLoopTests
+//
+//  Created by David Lourenço on 08/10/2024.
+//
+
+import XCTest
+
+final class PostDetailsUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
