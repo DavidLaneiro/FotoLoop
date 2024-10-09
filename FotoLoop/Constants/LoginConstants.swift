292a884d4adce3fb720e784ee401a8f5b8c1fe1f
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-09 13:20:46 +0100

    Add new files for login and register constants.

---
+++ FotoLoop/Constants/LoginConstants.swift
@@ -0,0 +1,8 @@
+//
+//  LoginConstants.swift
+//  FotoLoop
+//
+//  Created by David Lourenço on 09/10/2024.
+//
+
+import Foundation
---
+++ FotoLoop/Constants/RegisterConstants.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterConstants.swift
+//  FotoLoop
+//
+//  Created by David Lourenço on 09/10/2024.
+//
+
+import Foundation
