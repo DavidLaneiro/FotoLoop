d893d69a3bac5450a24cb7d599c80179262dd326
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-09 15:31:02 +0100

    Create enum class to manage navigation state.

---
+++ FotoLoop/Constants/NavigationEnum.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationEnum.swift
+//  FotoLoop
+//
+//  Created by David Lourenço on 09/10/2024.
+//
+
+import Foundation
+
+enum NavigationState{
+    // Simple enum to manage navigation state
+    case Login
+    case Register
+    case Feed
+    case CreatePost
+    case Profile
+    case EditProfile
+    
+}
