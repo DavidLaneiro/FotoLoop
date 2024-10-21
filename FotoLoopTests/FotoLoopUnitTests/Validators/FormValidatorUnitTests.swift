b1ff38a9476e38d440312fce1354e38f409f3ae4
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-21 16:43:44 +0100

    Add unit tests "blueprint" to FormValidator.

---
+++ FotoLoopTests/FotoLoopUnitTests/Validators/FormValidatorUnitTests.swift
@@ -0,0 +1,77 @@
+//
+//  FormValidatorUnitTests.swift
+//  FotoLoopTests
+//
+//  Created by David Lourenço on 21/10/2024.
+//
+
+import XCTest
+@testable import FotoLoop
+
+final class FormValidatorUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testValidUsernameFormat(){
+        
+    }
+    
+    func testInvalidUsernameFormat(){
+        
+    }
+    
+    func testValidEmailFormat(){
+        
+    }
+    
+    func testInvalidEmailFormat(){
+        
+    }
+    
+    func testValidFirstNameFormat(){
+        
+    }
+    
+    func testInvalidFirstNameFormat(){
+        
+    }
+    
+    func testValidLastNameFormat(){
+        
+    }
+    
+    func testInvalidLastNameFormat(){
+        
+    }
+    
+    func testValidPasswordFormat(){
+        
+    }
+    
+    func testInvalidPasswordFormat(){
+        
+    }
+    
+    func testValidRepeatPasswordFormat(){
+        
+    }
+    
+    func testInvalidRepeatPasswordFormat(){
+        
+    }
+    
+    func testValidForm(){
+        
+    }
+    
+    func testInvalidForm(){
+        
+    }
+
+}
