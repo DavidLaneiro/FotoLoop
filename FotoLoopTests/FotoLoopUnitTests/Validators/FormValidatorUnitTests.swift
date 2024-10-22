f485dad5607ceda79ecefea640017cd4ce4ece6b
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-22 15:37:24 +0100

    Remove register form declaration.

--- FotoLoopTests/FotoLoopUnitTests/Validators/FormValidatorUnitTests.swift
+++ FotoLoopTests/FotoLoopUnitTests/Validators/FormValidatorUnitTests.swift
@@ -13,7 +13,6 @@ import XCTest
 final class FormValidatorUnitTests: XCTestCase {
 
     var sut: FormValidator!
-    var registerForm: RegisterForm!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
