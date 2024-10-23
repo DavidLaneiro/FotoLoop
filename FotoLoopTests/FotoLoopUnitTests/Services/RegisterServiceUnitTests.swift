af0cca6f7578a90cf8c2f02866b72dec4c83f644
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-23 12:49:36 +0100

    Initial setup for RegisterService unit tests.

---
+++ FotoLoopTests/FotoLoopUnitTests/Services/RegisterServiceUnitTests.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterServiceUnitTests.swift
+//  FotoLoopTests
+//
+//  Created by David Lourenço on 23/10/2024.
+//
+
+import XCTest
+
+final class RegisterServiceUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+}
